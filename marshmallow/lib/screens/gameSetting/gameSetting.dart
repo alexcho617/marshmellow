b94b74e42ff38e7600a962da9919f697a9c2767e
alexcho617 <alexcho617@gmail.com>
2021-11-18 21:15:15 +0900

    fixed inital player loading error

--- marshmallow/lib/screens/gameSetting/gameSetting.dart
+++ marshmallow/lib/screens/gameSetting/gameSetting.dart
@@ -5,93 +5,85 @@ import 'package:marshmallow/utils/text.dart';
 import 'package:marshmallow/widgets/button.dart';
 import 'package:marshmallow/widgets/userMarshInfo.dart';
 
-
 class GameSettingPage extends StatefulWidget {
-
   @override
   State<GameSettingPage> createState() => _GameSettingPageState();
 }
 
 class _GameSettingPageState extends State<GameSettingPage> {
-  final List<String> _dropdownValues = [
-    "3",
-    "4",
-    "5",
-    "6",
-    "7"
-  ];
+  final List<String> _dropdownValues = ["3", "4", "5", "6", "7"];
   var _currentlySelected;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: backgroundBlue,
-      appBar: AppBar(
+        resizeToAvoidBottomInset: false,
         backgroundColor: backgroundBlue,
-        elevation: 0,
-        title: Image.asset('assets/logo.png', width: 95),
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 34),
-        child: Column(
-          children: [
-            SizedBox(height: 90),
-            Container(
-              width: 346,
-              decoration: new BoxDecoration(
-                color: yellow,
-                border: Border.all(color: darkGrey),
-                  borderRadius: new BorderRadius.only(
-                    topLeft: const Radius.circular(30.0),
-                    topRight: const Radius.circular(30.0),
-                )
-              ),
-              child: Column(
-                children: [
-                  SizedBox(height:20),
-                  Image.asset(
-                    'assets/m.png',
-                    width: 41,
-                  ),
-                  SizedBox(height:5),
+        appBar: AppBar(
+          backgroundColor: backgroundBlue,
+          elevation: 0,
+          title: Image.asset('assets/logo.png', width: 95),
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 34),
+          child: Column(
+            children: [
+              SizedBox(height: 90),
+              Container(
+                  width: 346,
+                  decoration: new BoxDecoration(
+                      color: yellow,
+                      border: Border.all(color: darkGrey),
+                      borderRadius: new BorderRadius.only(
+                        topLeft: const Radius.circular(30.0),
+                        topRight: const Radius.circular(30.0),
+                      )),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      Image.asset(
+                        'assets/m.png',
+                        width: 41,
+                      ),
+                      SizedBox(height: 5),
+                      Text(
+                        'RANDOM KEYWORDS',
+                        style: body3style(),
+                      ),
+                      SizedBox(height: 10),
+                      Container(
+                        width: 326,
+                        height: 229,
+                        decoration: new BoxDecoration(
+                          color: white.withOpacity(0.6),
+                          border: Border.all(color: darkGrey),
+                        ),
+                      ),
+                      TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            '🔄 다시 뽑기',
+                            style: body3style(),
+                          ))
+                    ],
+                  )),
+              SizedBox(height: 15),
+              Container(
+                width: 346,
+                height: 59,
+                decoration: new BoxDecoration(
+                  color: yellow,
+                  border: Border.all(color: darkGrey),
+                ),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                   Text(
-                    'RANDOM KEYWORDS',
+                    '인원 선택',
                     style: body3style(),
                   ),
-                  SizedBox(height:10),
-                  Container(
-                    width: 326,
-                    height: 229,
-                    decoration: new BoxDecoration(
-                      color: white.withOpacity(0.6),
-                      border: Border.all(color: darkGrey),
-                    ),
+                  SizedBox(
+                    width: 35,
                   ),
-                  TextButton(
-                    onPressed: (){}, 
-                    child: Text(
-                      '🔄 다시 뽑기',
-                      style: body3style(),
-                    )
-                  )
-                ],
-              )
-            ),
-            SizedBox(height:15),
-            Container(
-              width: 346,
-              height: 59,
-              decoration: new BoxDecoration(
-                color: yellow,
-                border: Border.all(color: darkGrey),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('인원 선택' , style: body3style(),),
-                  SizedBox(width: 35,),
                   Container(
-                    
                     width: 91,
                     height: 35,
                     alignment: Alignment.center,
@@ -99,15 +91,15 @@ class _GameSettingPageState extends State<GameSettingPage> {
                       color: white.withOpacity(0.6),
                       borderRadius: BorderRadius.circular(18),
                       border: Border.all(
-                        color: darkGrey, 
-                        style: BorderStyle.solid, 
-                        width: 1
-                      ),
+                          color: darkGrey, style: BorderStyle.solid, width: 1),
                     ),
                     child: DropdownButtonHideUnderline(
                       child: DropdownButton(
                         value: _currentlySelected,
-                        hint: Text('3', style: body3style(),),
+                        hint: Text(
+                          '3',
+                          style: body3style(),
+                        ),
                         items: _dropdownValues
                             .map((value) => DropdownMenuItem(
                                   child: Padding(
@@ -120,24 +112,27 @@ class _GameSettingPageState extends State<GameSettingPage> {
                         onChanged: (value) {
                           setState(() {
                             _currentlySelected = value;
+                            print(_currentlySelected);
                           });
                         },
                         isExpanded: false,
                         style: body3style(),
-                        
                       ),
                     ),
                   ),
                   SizedBox(width: 20),
-                  Text('명', style: body3style(),)
-                ]
+                  Text(
+                    '명',
+                    style: body3style(),
+                  )
+                ]),
               ),
-            ),
-            SizedBox(height:159),
-            mediumButtonTheme('방 만들기', (){})
-          ],
-        ),
-      )
-    );
+              // SizedBox(height:R109),
+              mediumButtonTheme('방 만들기', () {
+                //go to game zone
+              })
+            ],
+          ),
+        ));
   }
 }
--- marshmallow/lib/screens/home/home.dart
+++ marshmallow/lib/screens/home/home.dart
@@ -21,7 +21,7 @@ class _HomePageState extends State<HomePage> {
   late Future<String> uid;
   GameUser currentPlayer = GameUser();
   final _auth = FirebaseAuth.instance;
-
+  bool userLoaded = false;
   @override
   void initState() {
     super.initState();
@@ -33,7 +33,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     print(currentPlayer.globalToken);
-    
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: backgroundBlue,
@@ -47,25 +47,29 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: 48,),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Text('${currentPlayer.id}',
-                  style: head1style(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 1, bottom: 2),
-                  child: Text('님, 안녕하세요',
-                    style: body2style(),
-                  ),
-                ),
-                Spacer(),
-                
-              
-                UserMArshInfo(currentPlayer.globalToken)
-              ],
+            SizedBox(
+              height: 48,
             ),
+            userLoaded
+                ? Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        '${currentPlayer.id}',
+                        style: head1style(),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 1, bottom: 2),
+                        child: Text(
+                          '님, 안녕하세요',
+                          style: body2style(),
+                        ),
+                      ),
+                      Spacer(),
+                      UserMArshInfo(currentPlayer.globalToken ?? 0)
+                    ],
+                  )
+                : Text('User Loading'),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 30),
               child: Image.asset('assets/home.png'),
@@ -74,84 +78,71 @@ class _HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Container(
-                  width: 165,
-                  height: 218,
-                  child: OutlinedButton(
-                    
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '팀장',
-                          style: head1style()
-                        ),
-                        SizedBox(height:5),
-                        Text(
-                          '방 만들기',
-                          style: body1style()
-                        ),
-                      ],
-                    ),
-                    style: OutlinedButton.styleFrom (
-                      backgroundColor: yellow,
-                      side: BorderSide(color: darkGrey),
-                      shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(35),
+                    width: 165,
+                    height: 218,
+                    child: OutlinedButton(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('팀장', style: head1style()),
+                          SizedBox(height: 5),
+                          Text('방 만들기', style: body1style()),
+                        ],
                       ),
-                    ),
-                    onPressed: (){
-                      Get.to(() => GameSettingPage());
-                    },
-                  )
-                ),
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: yellow,
+                        side: BorderSide(color: darkGrey),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                      ),
+                      onPressed: () {
+                        Get.to(() => GameSettingPage());
+                      },
+                    )),
                 Container(
-                  width: 165,
-                  height: 218,
-                  child: OutlinedButton(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '팀원',
-                          style: head1style()
-                        ),
-                        SizedBox(height:5),
-                        Text(
-                          '참가하기',
-                          style: body1style()
-                        ),
-                      ],
-                    ),
-                    style: OutlinedButton.styleFrom (
-                      backgroundColor: yellow,
-                      side: BorderSide(color: darkGrey),
-                      shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(35),
+                    width: 165,
+                    height: 218,
+                    child: OutlinedButton(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('팀원', style: head1style()),
+                          SizedBox(height: 5),
+                          Text('참가하기', style: body1style()),
+                        ],
                       ),
-                    ),
-                    onPressed: () async {
-                      final String currentTeam = await _asyncInputDialog(context);},
-                  )
-                )
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: yellow,
+                        side: BorderSide(color: darkGrey),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(35),
+                        ),
+                      ),
+                      onPressed: () async {
+                        final String currentTeam =
+                            await _asyncInputDialog(context);
+                      },
+                    ))
               ],
             )
-            
           ],
         ),
       ),
       floatingActionButton: Container(
-        height: 74.0,
-        width: 74.0,
-        child: FittedBox(
-          child: FloatingActionButton(
+          height: 74.0,
+          width: 74.0,
+          child: FittedBox(
+              child: FloatingActionButton(
             onPressed: () {
               // Add your onPressed code here!
             },
-            child: Image.asset('assets/trophy.png', width: 40,),
+            child: Image.asset(
+              'assets/trophy.png',
+              width: 40,
+            ),
             backgroundColor: blue,
-          )
-        )
-      ),
+          ))),
     );
   }
 
@@ -176,30 +167,32 @@ class _HomePageState extends State<HomePage> {
         .doc(uid)
         .get()
         .then((DocumentSnapshot documentSnapshot) {
-      currentPlayer.id = documentSnapshot.get("id");
-      currentPlayer.avatarIndex = documentSnapshot.get("avatarIndex");
-      currentPlayer.globalToken = documentSnapshot.get("globalToken");
-      currentPlayer.localToken = documentSnapshot.get("localToken");
-      currentPlayer.uid = uid;
+      setState(() {
+        currentPlayer.id = documentSnapshot.get("id");
+        currentPlayer.avatarIndex = documentSnapshot.get("avatarIndex");
+        currentPlayer.globalToken = documentSnapshot.get("globalToken");
+        currentPlayer.localToken = documentSnapshot.get("localToken");
+        currentPlayer.uid = uid;
+        userLoaded = true;
+      });
     });
   }
 
   Future _asyncInputDialog(BuildContext context) async {
     String teamName = '';
-      
+
     return showDialog(
       context: context,
-      barrierDismissible: false, // dialog is dismissible with a tap on the barrier
+      barrierDismissible:
+          false, // dialog is dismissible with a tap on the barrier
       builder: (context) {
         return AlertDialog(
           titlePadding: EdgeInsets.zero,
           contentPadding: EdgeInsets.zero,
-
           backgroundColor: pink,
           shape: RoundedRectangleBorder(
-            
-            side: BorderSide(color: darkGrey),
-            borderRadius: BorderRadius.all(Radius.circular(30.0))),
+              side: BorderSide(color: darkGrey),
+              borderRadius: BorderRadius.all(Radius.circular(30.0))),
           content: Container(
             width: 346,
             height: 218,
@@ -208,14 +201,13 @@ class _HomePageState extends State<HomePage> {
               children: [
                 _getCloseButton(context),
                 Text('입장코드', style: head1style()),
-                SizedBox(height:15),
+                SizedBox(height: 15),
                 Container(
                   width: 266,
                   height: 69,
                   alignment: Alignment.center,
                   color: white.withOpacity(0.6),
                   child: TextField(
-                    
                     textAlign: TextAlign.center,
                     autofocus: false,
                     keyboardType: TextInputType.number,
@@ -224,17 +216,15 @@ class _HomePageState extends State<HomePage> {
                       border: InputBorder.none,
                       hintText: '네자리 숫자를 입력하세요',
                       hintStyle: body1style(),
-              
                     ),
                     style: body6style(),
-                    
                     onChanged: (value) {
                       teamName = value;
                     },
                   ),
                 ),
-                SizedBox(height:14),
-                smallButtonTheme('들어가기', (){})
+                SizedBox(height: 14),
+                smallButtonTheme('들어가기', () {})
               ],
             ),
           ),
@@ -242,18 +232,16 @@ class _HomePageState extends State<HomePage> {
       },
     );
   }
+
   _getCloseButton(context) {
     return Padding(
-      padding: const EdgeInsets.only(right:15),
+      padding: const EdgeInsets.only(right: 15),
       child: GestureDetector(
         child: Container(
           alignment: FractionalOffset.topRight,
           child: GestureDetector(
-            child: Icon(
-              Icons.clear,
-              color: darkGrey
-            ),
-            onTap: (){
+            child: Icon(Icons.clear, color: darkGrey),
+            onTap: () {
               Navigator.pop(context);
             },
           ),
@@ -262,4 +250,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
